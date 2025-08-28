866cfaee7136af367369de0bbcd545cdcd8ec9ef
Fiaz <fiaz89877@gamil.com>
2025-08-28 23:21:22 +0500

    setup app

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+export const login = (req, res) => {
+  console.log("Login User");
+};
+
+export const logout = (req, res) => {
+  console.log("Logout User");
+};
